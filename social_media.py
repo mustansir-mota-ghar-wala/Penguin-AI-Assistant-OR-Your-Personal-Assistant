6911be5363e4d93e53dbcbfed166f42f0dc74a9c
mustansir <mustanbhanpura87@gmail.com>
2026-02-19 17:02:08 +0530

    Initial commit - Voice Assistant Project

---
+++ social_media.py
@@ -0,0 +1,18 @@
+social = {
+    "google":"https://google.com",
+    "instagram" : "https://instagram.com",
+    "facebook":"https://facebook.com",
+    "linkdin":"https://linkedin.com",
+    "whatsapp":"https://whatsapp.com",
+    "youtube":"https://youtube.com",
+    "hotstar" :"https://hotstar.com"
+}
+
+song = {
+    "sapphire":"https://youtu.be/JgDNFQ2RaLQ?si=i1zm_lLBBwpkCfWX",
+    "perfect" : "https://youtu.be/2Vv-BfVoq4g?si=NbswCgt7NXNJ7zRY",
+    "kesariya":"https://youtu.be/g6fnFALEseI?si=DJtUtN7hKQ6e8Gza",
+    "mahi":"https://youtu.be/Etkd-07gnxM?si=w2Oa_8oWB4LL7cMt",
+    "zalima":"https://youtu.be/hhdSyBHuI88?si=VZjfFO2qMNCOdCHG",
+
+}
